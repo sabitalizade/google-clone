91b42a48cbab35f4800258cba91ed1ca0ac05757
sabitalizade <sabitalizade@gmail.com>
2021-07-30 17:28:52 +0400

    google ckone done

--- src/views/SearchScrean.js
+++ src/views/SearchScrean.js
@@ -7,19 +7,27 @@ import "../assets/css/searchScrean.css";
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
-
+// cx=017576662512468239146:omuauf_lfve
 const SearchScrean = () => {
+  const [results, setresults] = useState()
+  console.log(results)
   let query = useQuery();
   const [val, setVal] = useState(query.get("q"));
-  const q= query.get("q")
+  const q = query.get("q");
   const history = useHistory();
   useEffect(() => {
     axios
       .get(
-        `https://www.googleapis.com/customsearch/v1?key=AIzaSyAV0c8GJE3ooKHOs4xC_Fg_Qf2oa5Q_Pfs&cx=017576662512468239146:omuauf_lfve&q=${q}`
+        `https://www.googleapis.com/customsearch/v1?key=AIzaSyAV0c8GJE3ooKHOs4xC_Fg_Qf2oa5Q_Pfs&cx=11743e52e52cb525d&q=${q}`
       )
       .then((res) => {
         console.log(res.data);
+        setresults(res.data)
+        // return {
+        //   props:{
+        //     results:res.data
+        //   }
+        // }
       });
   }, [q]);
 
@@ -75,7 +83,27 @@ const SearchScrean = () => {
           </li>
         </ul>
       </div>
-      <div className="content"></div>
+      <div className="content">
+        <p>About {results?.searchInformation?.formattedTotalResults} results ({results?.searchInformation?.formattedSearchTime} seconds) </p>
+          {
+            results?.items?.map((result,id)=>(
+              <div className="item" key={id}>
+                
+                  <a href={result.link}>
+                  <div style={{color:"#333"}} className="trancate">
+                    {result.formattedUrl}
+
+                  </div>
+                  <div style={{fontSize:"20px"}} className="trancatetitle">{result.title}</div>
+                  </a>
+                
+                <p>
+                  {result.snippet}
+                </p>
+              </div>
+            ))
+          }
+      </div>
     </div>
   );
 };
